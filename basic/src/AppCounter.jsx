1d0f0551b6d142b2605178230920fd5ea82b8b1e
FEhyoeun <khyoeun92@gmail.com>
2023-07-23 19:28:48 +0900

    feat: 4.17, 18 카운터 상태 끌어올리기

--- basic/src/AppCounter.jsx
+++ basic/src/AppCounter.jsx
@@ -1,14 +1,21 @@
 import Counter from './components/Counter'
 import './App.css'
+import { useState } from "react";
 
 function AppCounter() {
+  const [totalCount, setTotalCount] = useState(0);
+  const handleTotalCount = () => setTotalCount((prev) => prev + 1)
 
-
-    return (
-        <>
-            <Counter />
-        </>
-    );
+  return (
+    <>
+      <div className='total-count'>
+        <span className='total-count-title'>Total Count: {totalCount}</span>
+        <span className='total-count-desc'>{totalCount < 10 ? '❄' : '🎇'}</span>
+      </div>
+      <Counter totalCount={totalCount} handleTotalCount={handleTotalCount}/>
+      <Counter totalCount={totalCount} handleTotalCount={handleTotalCount}/>
+    </>
+  );
 }
 
 export default AppCounter;
--- basic/src/components/Counter.jsx
+++ basic/src/components/Counter.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
-    const handleClick = () => setCount(count + 1)
+export default function Counter({ totalCount, handleTotalCount }) {
+  const [count, setCount] = useState(0);
+  const handleCount = () => setCount((prev) => prev + 1)
+  const handleClickEvent = () => {
+    handleCount()
+    handleTotalCount()
+  }
 
-    return (
-        <div className='counter'>
-            <span className='count'>{ count }</span>
-            <button className='add-button' onClick={ handleClick }>Add</button>
-        </div>
-    )
+  return (
+    <div className='counter'>
+      <span className='count'>{count} / {totalCount}</span>
+      <button className='add-button' onClick={handleClickEvent}>Add</button>
+    </div>
+  )
 }
