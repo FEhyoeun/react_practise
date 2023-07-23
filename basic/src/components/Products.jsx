80ec6707a65e443680b04a5eac053d1167e619a3
FEhyoeun <khyoeun92@gmail.com>
2023-07-23 20:00:53 +0900

    feat: 4.20 useEffect 제대로 사용하기

--- basic/src/components/Products.jsx
+++ basic/src/components/Products.jsx
@@ -3,27 +3,34 @@ import React, { useEffect, useState } from 'react';
 function Products() {
   const [count, setCount] = useState(0)
   const [products, setProducts] = useState([])
+  const [checked, setChecked] = useState(false)
+  const handleChange = () => setChecked((prev) => !prev)
 
   useEffect(() => {
-    fetch('data/products.json')
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => setProducts(data))
     return () => {
       console.log('unmounted')
     }
-  }, []);
+  }, [checked]);
 
   return (
     <>
-      {products.map((product) => (
-        <ul key={product.id}>
-          <article>
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
-          </article>
-        </ul>
-      ))}
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+      <input id='checkbox' type='checkbox' value={checked} onChange={handleChange}/>
+      <label htmlFor='checkbox'>Show Only Hot Sale</label>
+
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+      )
     </>
   );
 }
