21ade30df6573ecc988f28e05794a80b314a07be
FEhyoeun <khyoeun92@gmail.com>
2023-08-09 20:58:38 +0900

    feat: 5.15 컴포넌트의 재사용1(강의)

--- basic/src/AppWrap.jsx
+++ basic/src/AppWrap.jsx
@@ -3,19 +3,21 @@ import React from 'react';
 export default function AppWrap() {
   return (
     <>
-      <Navbar/>
+      <Navbar>
+        <Avatar
+          image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
+          name='Bob'
+          size={200}
+        />
+      </Navbar>
     </>
   );
 }
 
-function Navbar() {
+function Navbar({ children }) {
   return (
     <header style={{ backgroundColor: 'yellow' }}>
-      <Avatar
-        image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
-        name='Bob'
-        size={200}
-      />
+      {children}
     </header>
 
   )
