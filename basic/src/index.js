3aa8b2533fdfdc979cf0b4255ca9642339462a29
FEhyoeun <khyoeun92@gmail.com>
2023-08-09 20:55:06 +0900

    feat: 5.15 컴포넌트의 재사용1(default)

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AppForm';
+import App from './AppWrap';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
